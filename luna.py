db4c87f21d3e0d7cb8ea45ca2fa15dda7a92cb41
BuildTools <unconfigured@null.spigotmc.org>
2020-03-28 19:59:11 +0000

    - Added text to speech

--- luna.py
+++ luna.py
@@ -1,9 +1,13 @@
+import msvcrt
 import sys
 import requests
 import config
 from datetime import datetime
 from termcolor import colored, cprint
 import json
+import pyttsx3
+import time
+
 
 # Create LIFX class
 class lifx:
@@ -14,7 +18,7 @@ class lifx:
     @staticmethod
     def turnOff():
         # Print response
-        cprint("Turning off the lights...", "magenta")
+        lunaSay("Turning off the lights...")
 
         # Give authorisation
         headers = {
@@ -33,7 +37,7 @@ class lifx:
     @staticmethod
     def turnOn():
         # Print response
-        cprint("Turning on the lights...", "magenta")
+        lunaSay("Turning on the lights...")
 
         # Give authorisation
         headers = {
@@ -51,7 +55,7 @@ class lifx:
     @staticmethod
     def setColour(colour):
         # Print response
-        cprint("Changing colour of lights to " + colour + "...", "magenta")
+        lunaSay("Changing colour of lights to " + colour + "...")
 
         # Give authorisation
         headers = {
@@ -67,8 +71,11 @@ class lifx:
         # Send request
         response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
 
+
+# Create weather class
 class weather:
 
+    # Method to return current weather information in your area
     @staticmethod
     def currentWeather():
         # Find city
@@ -76,7 +83,8 @@ class weather:
         city = ipData["city"]
 
         # Request current weather information
-        cWeather = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + config.openWeatherKey)
+        cWeather = requests.get(
+            "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + config.openWeatherKey)
 
         # Store weather information
         cWeatherJSON = json.loads(cWeather.text)
@@ -90,7 +98,8 @@ class weather:
         cWeatherTemp = str(round(cWeatherMain.get("temp") - 273.15))
 
         # Print reponse
-        cprint("The weather in " + city + " is " + cWeatherDesc + " with a temperature of " + cWeatherTemp + chr(176) + "C", "magenta")
+        lunaSay("The weather in " + city + " is " + cWeatherDesc + " with a temperature of " + cWeatherTemp + chr(
+            176) + "C")
 
 # Declare method to get command from user
 def getCommand():
@@ -101,10 +110,11 @@ def getCommand():
 
 # Declare method to close program
 def exitLuna():
-    cprint("Exiting...", "magenta")
+    lunaSay("Exiting...")
     sys.exit(0)
 
 
+# Declare method to return the current date
 def getDate():
     time = datetime.now().time().strftime("%H:%M")
     date = datetime.today().strftime("%d/%m/%Y")
@@ -112,14 +122,37 @@ def getDate():
     return "The time is: " + time + " on the " + date
 
 
+# Declare method to allow Luna to talk
+def lunaSay(text):
+    # Initalise tts engine
+    ttsEngine = pyttsx3.init()
+
+    # Set Luna's vocie
+    voices = ttsEngine.getProperty("voices")
+    ttsEngine.setProperty("voice", voices[1].id)
+
+    # Set Luna's volume
+    ttsEngine.setProperty("volume", 0.4)
+
+    # Changing Luna's rate
+    rate = ttsEngine.getProperty("rate")
+    ttsEngine.setProperty("rate", rate + 50)
+
+    # Say and print Luna's message
+    cprint(text, "magenta")
+    ttsEngine.say(text)
+    ttsEngine.runAndWait()
+
+
 # Declare main loop
 def main():
-    # Display message
+
+    # Prompt for input
     if config.firstBoot:
         config.firstBoot = False
-        cprint("Hi! I'm Luna! How may I be of service today:", "magenta")
+        lunaSay("Hi! I'm Luna! How may I be of service today")
     else:
-        cprint("Anything else?", "magenta")
+        lunaSay("Anything else?")
 
     # Get command from user
     command = getCommand().lower()
@@ -130,10 +163,11 @@ def main():
     elif command.__contains__("turn on") and command.__contains__("lights"):
         lifx.turnOn()
     elif command.__contains__("time") or command.__contains__("date"):
-        cprint(getDate(), "magenta")
+        lunaSay(getDate())
     elif command.startswith("no") or command.startswith("close") or command.startswith("exit"):
         exitLuna()
-    elif command.__contains__("colour") or command.__contains__("color") or command.__contains__("set") and command.__contains__("lights"):
+    elif command.__contains__("colour") or command.__contains__("color") or command.__contains__(
+            "set") and command.__contains__("lights"):
         if command.__contains__("pink"):
             lifx.setColour("pink")
         elif command.__contains__("blue"):
@@ -154,10 +188,11 @@ def main():
         if command.__contains__("today"):
             weather.currentWeather()
     else:
-        cprint("I'm sorry, I don't understand.", "magenta")
+        lunaSay("I'm sorry, I don't understand.")
 
     # Re ask
     main()
+
 
 # Start the program
 main()
